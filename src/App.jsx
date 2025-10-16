8b568c2981a1460f0b1881a83cf2347bb4074bb8
SarfrazHassan095 <hmsh4090@gmail.com>
2025-10-16 09:43:40 +0500

    Add note functionality is working

--- src/App.jsx
+++ src/App.jsx
@@ -5,37 +5,85 @@ import "./App.css";
 // import Card from "./learning/components/contact-list/card";
 // import contactList from "./learning/utils/contscts";
 // import EmojiPediaApp from "./learning/components/emojiApp/Main";
-import Login from "./learning/components/loginApp/Login";
+// import Login from "./learning/components/loginApp/Login";
 
 // Keeper app components
+import { useState } from "react";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Note from "./components/Note";
-import notes from "./components/notes";
+import initialNotes from "./components/notes";
 function App() {
+  const [notes, setNotes] = useState(initialNotes);
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("title", title);
+    console.log("content", content);
+
+    if (!title || !content) return alert("Please enter title and content");
+    if (title && content) {
+      setNotes([...notes, { key: notes.length + 1, title, content }]);
+
+      console.log("notes array, ", notes);
+      setTitle("");
+      setContent("");
+    }
+  };
   {
     /* <div className="w-11/12 mx-auto mt-6 grid grid-cols-3 gap-3"> */
   }
   return (
-    // <div className="h-screen overflow-auto bg-slate-100">
-    //   <Header />
+    <div className="h-screen overflow-auto bg-slate-100">
+      <Header />
 
-    //   <div className="w-11/12 mx-auto mt-6 flex justify-evenly flex-wrap gap-3">
-    //     {notes.map((note) => (
-    //       <Note key={note.key} title={note.title} content={note.content} />
-    //     ))}
-    //   </div>
+      <form
+        className="relative w-1/2 lg:w-1/3 mx-auto bg-white p-5 rounded-md my-5"
+        onSubmit={handleSubmit}
+      >
+        <input
+          type="text"
+          placeholder="Title"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+          className="w-full h-10 px-2 font-semibold shadow-md rounded-md mb-3 focus:outline-yellow-500 focus:outline-2"
+        />
+        <textarea
+          name=""
+          id=""
+          placeholder="Content"
+          rows={3}
+          value={content}
+          onChange={(e) => setContent(e.target.value)}
+          className="w-full p-2 font-light shadow-md rounded-md focus:outline-yellow-500 focus:outline-2"
+        ></textarea>
+        <button
+          type="submit"
+          className="absolute right-3 -bottom-5 h-10 w-10 rounded-full  bg-yellow-500 text-white flex justify-center items-center"
+        >
+          Add
+        </button>
+      </form>
 
-    //   <div className="absolute w-full bottom-0 ">
-    //     <Footer />
-    //   </div>
-    // </div>
-    <Login />
+      <div className="w-11/12 mx-auto mt-6 flex justify-evenly flex-wrap gap-3">
+        {notes.map((note) => (
+          <Note key={note.key} title={note.title} content={note.content} />
+        ))}
+      </div>
+
+      <div className="absolute w-full bottom-0 ">
+        <Footer />
+      </div>
+    </div>
   );
 }
 
 {
   /* Learning components */
+}
+{
+  /* <Login /> */
 }
 {
   /* <Header />
--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,6 +1,9 @@
-export default function Note({ title, content }) {
+export default function Note({ id, title, content }) {
   return (
-    <div className="py-1 px-2 md:p-3 rounded-lg bg-white shadow-lg w-60 float-left ">
+    <div className="relative py-1 px-2 md:p-3 rounded-lg bg-white shadow-lg w-60 float-left ">
+      <button className="absolute right-3 top-3 opacity-50 cursor-pointer">
+        ❌
+      </button>
       <div className="text-md text-slate-800 font-semibold">{title}</div>
       <div className="text-sm font-light text-slate-600">{content}</div>
     </div>
